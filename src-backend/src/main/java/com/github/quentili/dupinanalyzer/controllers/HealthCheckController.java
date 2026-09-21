e2347b83aecf87bd052e1a9d7a0284c5ca0e67cd
Quentili <episodnik@gmail.com>
2026-09-21 11:16:10 +0200

    added: health checker

---
+++ src-backend/src/main/java/com/github/quentili/dupinanalyzer/controllers/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.github.quentili.dupinanalyzer.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> onHealth() {
+        return ResponseEntity.ok().build();
+    }
+}
